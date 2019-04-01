d4f3db10f428b58736c1825009e3842aa9c23886
pavlo <pavelnemoi@gmail.com>
2019-04-01 19:20:18 +0300

    Battle service: implement duel action

--- source/battle/app/Algorithm/Dice.php
+++ source/battle/app/Algorithm/Dice.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+namespace App\Algorithm;
+
 final class Dice
 {
     public const TOTAL_ROUNDS = 3;
--- source/battle/app/Http/Controllers/BattleController.php
+++ source/battle/app/Http/Controllers/BattleController.php
@@ -4,13 +4,27 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Algorithm\Dice;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 final class BattleController extends Controller
 {
+    private $dice;
+
+    public function __construct(Dice $dice)
+    {
+        $this->dice = $dice;
+    }
+
     public function duel(Request $request): JsonResponse
     {
-        return response()->json([]);
+        $duel = $this->dice->fight();
+
+        return response()->json([
+            'player1' => $request->input('userA'),
+            'player2' => $request->input('userB'),
+            'duelResults' => $duel
+        ]);
     }
 }
