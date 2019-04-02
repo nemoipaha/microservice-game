f580d4919fdb94934c0638e0c3f342ebcf4a0db1
pavlo <pavelnemoi@gmail.com>
2019-04-02 14:56:34 +0300

    Secret service: add secrets table migration

---
+++ source/secret/database/migrations/2019_04_02_115033_create_secrets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSecretsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('secrets', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('name');
+            $table->double('latitude');
+            $table->double('longitude')->nullable();
+            $table->string('location_name', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('secrets');
+    }
+}
