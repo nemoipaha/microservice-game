a24041a905f5ed33335c78686b2bd6a86d217786
pavlo <pavelnemoi@gmail.com>
2019-07-12 16:51:47 +0300

    User service:  add user migration

---
+++ source/user/database/migrations/2019_07_10_160021_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function(Blueprint $blueprint) {
+            $blueprint->bigIncrements('id');
+            $blueprint->string('email')->unique();
+            $blueprint->string('password');
+            $blueprint->string('api_token')->nullable();
+            $blueprint->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
